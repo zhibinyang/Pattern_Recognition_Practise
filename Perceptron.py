b93688c95f537374ad8321181af0531c6354827f
zhibinyang <yang_zhibin824@qq.com>
2016-12-23 15:40:56 +0800

    Add Exception to the model that not working

--- Perceptron.py
+++ Perceptron.py
@@ -3,7 +3,7 @@ import numpy as np
 # Algorithm: w(t+1) = w(t) - rho * x_t, if x_t in omega_2 and w^T(t)x_t >= 0
 
 def getTrainingDataSet_1():
-    return np.array([[-1,0,1],[0,1,1]])
+    return np.array([[-1,0,1],[0,1,1],[1,-1,1]])
 
 def getTrainingDataSet_2():
     return np.array([[0,-1,1],[1,0,1]])
@@ -12,28 +12,31 @@ def getInitW():
     return np.array([0,0,0])
 
 def Perceptron(DataSet1, DataSet2, InitW, rho):
+    W = InitW
     ok_count = 0
     recurring_count = 0
     total_count = DataSet1.shape[0] + DataSet2.shape[0]
     while True:
         for row in DataSet1:
             recurring_count += 1
-            if row.dot(InitW) <= 0:
-                InitW = InitW + row * rho
+            if row.dot(W) <= 0:
+                W = W + row * rho
                 ok_count = 0
             else:
                 ok_count += 1
         for row in DataSet2:
             recurring_count += 1
-            if row.dot(InitW) >= 0:
-                InitW = InitW - row * rho
+            if row.dot(W) >= 0:
+                W = W - row * rho
                 ok_count = 0
             else:
                 ok_count += 1
         if ok_count >= total_count:
             break
-    print InitW
-    print recurring_count
+        if recurring_count >= 10000:
+            raise BaseException("Running more than 10000 time, aborting...")
+    print "Final W:",W
+    print "Recurring Count:",recurring_count
 
 def main():
     DataSet1 = getTrainingDataSet_1()
